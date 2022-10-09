6783e79ade8ddac6f6ddf74aa6e7f24f1310540c
kakarot786 <81684205+kakarot786@users.noreply.github.com>
2022-10-09 14:38:33 +0500

    Update lab_task_1.java

--- lab_task_1.java
+++ lab_task_1.java
@@ -2,9 +2,12 @@ import java.util.*;
 
 class lab_task_1 {
 	public static void main(String[] args){
+              // Making a New Object For Taking User Input.
 		Scanner obj = new Scanner(System.in);
+              // Taking User Input.
 		System.out.println("Enter Any Number: ");
 		int num = obj.nextInt();
+              // Applying Logic.
 		if(num%2 == 0)
 		{
 			System.out.println("Number is Even");
