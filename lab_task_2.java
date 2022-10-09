ae09bfa8d47a802230e8d7827a8f8915125bb025
kakarot786 <81684205+kakarot786@users.noreply.github.com>
2022-10-09 14:40:56 +0500

    Update lab_task_2.java

--- lab_task_2.java
+++ lab_task_2.java
@@ -4,14 +4,17 @@ class lab_task_2 {
 	public static void main(String[] args){
 		float a,b,add,subtract,divide,multiply,modulus;
 		Scanner obj = new Scanner(System.in);
+              // Taking 2 User Inputs.
 		System.out.println("Enter Any 2 Numbers: ");
 		a = obj.nextInt();
 		b = obj.nextInt();
+              // Performing Operations By Operators.
 		add = a + b;
 		subtract = a - b;
 		multiply = a * b;
 		divide = a / b;
 		modulus = a % b;
+              // Printing Out The Results.
 		System.out.println("Addition of " + a + " and " + b + " = " + add);
 		System.out.println("Subtraction of " + a + " and " + b + " = " + subtract);
 		System.out.println("Multiplication of " + a + " and " + b + " = " + multiply);
