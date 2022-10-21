1e49d74aa7ba3af14441b2a884893b89eefa78dc
kakarot786 <lordbeerus870@gmail.com>
2022-10-21 08:17:20 -0700

    Adding the last task

---
+++ lab_task_6.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class lab_task_6{
+	public static void main(String[] args){
+		int a;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome To Our Restaurant! Press The Following Codes For Menu Item Prices: \n1) Chicken Borgir\n2) Beef Borgir\n3) Cheese Borgir\n4)Pizza\n5)Fries");
+		int a = sc.nextInt();
+		switch(a){
+			case 1:
+				System.out.println("Chicken Borgir = Rs.100");
+				break;
+			case 2:
+				System.out.println("Beef Borgir = Rs.200");
+				break;
+			case 3:
+				System.out.println("Cheese Borgir = Rs.300");
+				break;
+			case 4:
+				System.out.println("Pizza = Rs.400");
+				break;
+			case 5:
+				System.out.println("Fries = Rs.50");
+				break;
+			default:
+			System.out.println("Please Select Items From 1 to 5");												
+		}
+	}
+}
