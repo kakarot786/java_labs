d85955e2f0702fb361ea2f4a8485e1448e3a4700
kakarot786 <lordbeerus870@gmail.com>
2022-10-22 01:25:34 -0700

    changing percentage to marks

--- lab_task_3.java
+++ lab_task_3.java
@@ -4,16 +4,16 @@ class lab_task_3 {
 	public static void main(String[] args){
 		float matric,inter,entry,cpn;
 		Scanner obj = new Scanner(System.in);
-              // Taking First User Input
+              // Taking First User Input.
 		System.out.println("Enter Your Matriculation Marks: ");
 		matric = obj.nextFloat();
-              // Taking Second User Input
+              // Taking Second User Input.
 		System.out.println("Enter Your Intermediate Marks: ");
 		inter = obj.nextFloat();
-              // Taking Third User Input
+              // Taking Third User Input.
 		System.out.println("Enter Your Entry Test Marks: ");
 		entry = obj.nextFloat();
-              // Applying Formula
+              // Applying Formula.
 			  float matric_percent = (matric/850)*100;
 			  float inter_percent = (inter/1100)*100;
 			  float entry_percent = (entry/100)*100;
