0a9861c1f19eb49e60ca09bdf3ece08795c14af8
kakarot786 <lordbeerus870@gmail.com>
2022-10-22 02:26:16 -0700

    Making Final Changes

--- lab_task_3.java
+++ lab_task_3.java
@@ -20,7 +20,26 @@ class lab_task_2 {
 		System.out.println("Multiplication of " + a + " and " + b + " = " + multiply);
 		System.out.println("Division of " + a + " and " + b + " = " + divide);
 		System.out.println("Modulus of " + a + " and " + b + " = " + modulus);
-
-
+		System.out.println
+		("\"Using Relational Operators\"");
+		System.out.print("if a is greater than b prints true otherwise false: ");
+		System.out.println(a>b);
+		System.out.print("if a is greater than or equals to b prints true otherwise false: ");
+		System.out.println(a>=b);
+		System.out.print("if a is less than b prints true otherwise false: ");
+		System.out.println(a<b);
+		System.out.print("if a is less than or equals to b prints true otherwise false: ");
+		System.out.println(a<=b);
+		System.out.print("if a is equals to b prints true otherwise false: ");
+		System.out.println(a == b);
+		System.out.print("if a is not equals to b prints true otherwise false: ");
+		System.out.println(a!=b);
+		System.out.println("\"Using Logical Operators\"");
+		if(a > 5 && a <10){
+			System.out.println("Condition is true in terms of AND operator");
+		}
+		if(a > 5 || a < 50){
+			System.out.println("Condition is true in terms of OR operator");
+		}
 	}
 }
