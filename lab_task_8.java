d468dfe9f2a32402cd2fbcaf42523eb372f3c7f4
kakarot786 <lordbeerus870@gmail.com>
2022-10-26 13:17:34 -0700

    improvising

--- lab_task_8.java
+++ lab_task_8.java
@@ -4,16 +4,18 @@ import java.util.*;
 class lab_task_8{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
+		char choice = 'n';
 		//Taking Input
 		System.out.println("Enter First & Second Number: ");
 		double num1 = sc.nextDouble();
 		double num2 = sc.nextDouble();
+		do{
 		//Asking user to select the operation.
 		System.out.println("Which Arithmetic Operation Would You Like To Perform?");
 		System.out.println("Press: \n1) for Division\n2) for Multiplication\n3) for Addition\n4) for Subtraction");
 		int operation = sc.nextInt();
 		double calc = 0;
-		//Using switch to perform the selected operation.
+		//Using switch to perform the selected operation.	
 		switch(operation){
 			case 1:
 				calc = num1/num2;
@@ -35,7 +37,9 @@ class lab_task_8{
 				System.out.println("Wrong Numnber Entered!");
 
 
-		}
-
+			}
+			System.out.println("Would you like to perform any other operation? (y/n)");
+			choice = sc.next().charAt(0);	
+		}while(choice == 'y' || choice == 'Y');
 	}
 }
